97122fdf4b94e8ba3ae13d17f2b2c68294aad274
Rakshit <rakshityadav411@gmail.com>
2024-01-28 23:39:15 +0530

    lowkey done

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import requests
 
 html_text=requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=as&searchTextText=Python&txtKeywords=Python&txtLocation=').text
 soup=BeautifulSoup(html_text, 'lxml')
-jobs=soup.find('li', class_='clearfix job-bx wht-shd-bx')
-company_name=jobs.find('h3',class_='joblist-comp-name').text.replace(' ','')
-skills=jobs.find('span', class_='srp-skills').text.replace(' ','')
-print(skills)
-print(company_name)
+jobss=soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+for jobs in jobss:
+    published_date=jobs.find('span',class_='sim-posted').span.text
+    if 'few' in published_date:
+        with open('posts/{jobs}.txt','w') as f:
+            
+            company_name=jobs.find('h3',class_='joblist-comp-name').text.replace(' ','')
+            skills=jobs.find('span', class_='srp-skills').text.replace(' ','')
+            f.write(f'''
+            Company Name:{company_name}
+            Required Skills:{skills}
+            ''')
+            f.write('')
