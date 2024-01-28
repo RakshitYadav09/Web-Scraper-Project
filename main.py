b2380e9c5ef1ec0d5fc4fc7065a11f91f9625e41
Rakshit <rakshityadav411@gmail.com>
2024-01-28 23:19:43 +0530

    web scraper tool

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open ('Allinone _ Web Scraper Test Sites.html', 'r') as html_file:
-    content=html_file.read()
-    
-    soup=BeautifulSoup(content, 'lxml')
-    tags=soup.find_all('h4')
-    print(tags)
+html_text=requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=as&searchTextText=Python&txtKeywords=Python&txtLocation=').text
+soup=BeautifulSoup(html_text, 'lxml')
+jobs=soup.find('li', class_='clearfix job-bx wht-shd-bx')
+company_name=jobs.find('h3',class_='joblist-comp-name').text.replace(' ','')
+skills=jobs.find('span', class_='srp-skills').text.replace(' ','')
+print(skills)
+print(company_name)
